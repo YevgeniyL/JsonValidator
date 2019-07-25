5ffb2d943f117305e22bafbd8b5f37fe02cd20a2
Steklorez <johnnykforever@gmail.com>
2019-07-25 14:37:12 +0300

    Add tests

--- src/test/java/io/polybius/testtask/validator/JsonValidatorTest.java
+++ src/test/java/io/polybius/testtask/validator/JsonValidatorTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.BlockJUnit4ClassRunner;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -29,20 +30,23 @@ public class JsonValidatorTest {
         JsonObject obj1 = new JsonObject();
         obj1.add(jsonName, new JsonPrimitive(jsonValue));
         list.add(obj1);
-
         List<JsonObject> result = new JsonDataValidator().run(query, list);
+
         Assert.assertEquals(1, result.size());
         final JsonObject jsonObject = result.get(0);
         Assert.assertTrue(jsonObject.has(jsonName));
         Assert.assertEquals(jsonValue, jsonObject.get(jsonName).getAsString());
     }
 
+    /**
+     * For strings json need to contain a value from query ignore case
+     */
     @Test
     public void testEqualityForString() {
-        final String query = "name=Bobby";
+        final String query = "name=BoB";
         final String fieldName = "name";
-        final String valid1 = "Bobby";
-        final String valid2 = "BoB";
+        final String valid1 = "BoB";
+        final String valid2 = "Bobby";
         final String notValid = "Rob";
         JsonObject jsonObj1 = new JsonObject();
         jsonObj1.add(fieldName, new JsonPrimitive(valid1));
@@ -51,8 +55,8 @@ public class JsonValidatorTest {
         JsonObject jsonObj3 = new JsonObject();
         jsonObj3.add(fieldName, new JsonPrimitive(notValid));
         List<JsonObject> list = new LinkedList<>(Arrays.asList(jsonObj1, jsonObj2, jsonObj3));
-
         List<JsonObject> result = new JsonDataValidator().run(query, list);
+
         Assert.assertEquals(2, result.size());
         final JsonObject jsonObject = result.get(0);
         Assert.assertTrue(jsonObject.has(fieldName));
@@ -66,119 +70,172 @@ public class JsonValidatorTest {
     @Test
     public void testEqualityForNumber() {
         final String query = "age=10";
-        final String fieldName = "age";
-        final int valid1 = 10;
-        final double valid2 = 10.0;
-        final long notValid1 = 11;
-        final double notValid2 = 11.0;
-        JsonObject jsonObj1 = new JsonObject();
-        jsonObj1.add(fieldName, new JsonPrimitive(valid1));
-        JsonObject jsonObj2 = new JsonObject();
-        jsonObj2.add(fieldName, new JsonPrimitive(valid2));
-        JsonObject jsonObj3 = new JsonObject();
-        jsonObj3.add(fieldName, new JsonPrimitive(notValid1));
-        JsonObject jsonObj4 = new JsonObject();
-        jsonObj3.add(fieldName, new JsonPrimitive(notValid2));
-        List<JsonObject> list = new LinkedList<>(Arrays.asList(jsonObj1, jsonObj2, jsonObj3, jsonObj4));
+        final String fieldAge = "age";
+        final int valueInt = 10;
+        List<JsonObject> result = getValidatedIntData(query, fieldAge, valueInt);
+        Assert.assertEquals(1, result.size());
+        JsonObject jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(valueInt, jsonObject.get(fieldAge).getAsInt());
 
-        List<JsonObject> result = new JsonDataValidator().run(query, list);
-        Assert.assertEquals(2, result.size());
-        final JsonObject jsonObject = result.get(0);
-        Assert.assertTrue(jsonObject.has(fieldName));
-        Assert.assertEquals(valid1, jsonObject.get(fieldName).getAsInt());
-
-        final JsonObject jsonObject2 = result.get(1);
-        Assert.assertTrue(jsonObject2.has(fieldName));
-        Assert.assertEquals(valid2, jsonObject2.get(fieldName).getAsDouble(), 2);
+        final double valueDouble = 10.0;
+        result = getValidatedDoubleData(query, fieldAge, valueDouble);
+        Assert.assertEquals(1, result.size());
+        jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(valueDouble, jsonObject.get(fieldAge).getAsDouble(), 2);
     }
 
     @Test
-    public void testGtOrEq() {
+    public void testEqualityNumberForWrongInt() {
+        List<JsonObject> result = getValidatedIntData("age=10", "age", 9);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testEqualityNumberForWrongDouble() {
+        List<JsonObject> result = getValidatedDoubleData("age=10", "age", 10.1);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testGreaterThanOrEqual() {
         final String query = "age>=10";
-        final String fieldName = "age";
-        final int jsonValue1 = 10;
-        final int jsonValue2 = 20;
-        final int jsonValue3 = 9;
-        JsonObject jsonObj1 = new JsonObject();
-        jsonObj1.add(fieldName, new JsonPrimitive(jsonValue1));
-        JsonObject jsonObj2 = new JsonObject();
-        jsonObj2.add(fieldName, new JsonPrimitive(jsonValue2));
-        JsonObject jsonObj3 = new JsonObject();
-        jsonObj3.add(fieldName, new JsonPrimitive(jsonValue3));
-        List<JsonObject> list = new LinkedList<>(Arrays.asList(jsonObj1, jsonObj2, jsonObj3));
+        final String fieldAge = "age";
+        int value = 10;
+        List<JsonObject> result = getValidatedIntData(query, fieldAge, value);
+        Assert.assertEquals(1, result.size());
+        JsonObject jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(value, jsonObject.get(fieldAge).getAsInt());
 
-        List<JsonObject> result = new JsonDataValidator().run(query, list);
-        Assert.assertEquals(2, result.size());
-        final JsonObject jsonObject = result.get(0);
-        Assert.assertTrue(jsonObject.has(fieldName));
-        Assert.assertEquals(jsonValue1, jsonObject.get(fieldName).getAsInt());
+        value = 11;
+        result = getValidatedIntData(query, fieldAge, value);
+        Assert.assertEquals(1, result.size());
+        jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(value, jsonObject.get(fieldAge).getAsInt());
 
-        final JsonObject jsonObject2 = result.get(1);
-        Assert.assertTrue(jsonObject2.has(fieldName));
-        Assert.assertEquals(jsonValue2, jsonObject2.get(fieldName).getAsInt());
+        double doubleValue = 10.1;
+        result = getValidatedDoubleData(query, fieldAge, doubleValue);
+        Assert.assertEquals(1, result.size());
+        jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(doubleValue, jsonObject.get(fieldAge).getAsDouble(), 2);
     }
 
     @Test
-    public void testGt() {
+    public void testGreaterThanOrEqualForWrongInt() {
+        List<JsonObject> result = getValidatedIntData("age>=10", "age", 9);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testGreaterThanOrEqualForWrongDouble() {
+        List<JsonObject> result = getValidatedDoubleData("age>=10", "age", 9.9);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testGreaterThan() {
         final String query = "age>10";
-        final String fieldName = "age";
-        final int jsonValue1 = 11;
-        final int jsonValue2 = 10;
-        JsonObject jsonObj1 = new JsonObject();
-        jsonObj1.add(fieldName, new JsonPrimitive(jsonValue1));
-        JsonObject jsonObj2 = new JsonObject();
-        jsonObj2.add(fieldName, new JsonPrimitive(jsonValue2));
-        List<JsonObject> list = new LinkedList<>(Arrays.asList(jsonObj1, jsonObj2));
+        final String fieldAge = "age";
+        final int value = 11;
+        List<JsonObject> result = getValidatedIntData(query, fieldAge, value);
 
-        List<JsonObject> result = new JsonDataValidator().run(query, list);
         Assert.assertEquals(1, result.size());
-        final JsonObject jsonObject = result.get(0);
-        Assert.assertTrue(jsonObject.has(fieldName));
-        Assert.assertEquals(jsonValue1, jsonObject.get(fieldName).getAsInt());
+        JsonObject jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(value, jsonObject.get(fieldAge).getAsInt());
+
+        double doubleValue = 10.1;
+        result = getValidatedDoubleData(query, fieldAge, doubleValue);
+        Assert.assertEquals(1, result.size());
+        jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(doubleValue, jsonObject.get(fieldAge).getAsDouble(), 2);
     }
 
     @Test
-    public void testLtOrEq() {
+    public void testGreaterThanForWrongInt() {
+        List<JsonObject> result = getValidatedIntData("age>10", "age", 10);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testGreaterThanForWrongDouble() {
+        List<JsonObject> result = getValidatedDoubleData("age>10", "age", 9.9);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testLessThanOrEqual() {
         final String query = "age<=10";
-        final String fieldName = "age";
-        final int jsonValue1 = 10;
-        final int jsonValue2 = 9;
-        final int jsonValue3 = 11;
-        JsonObject jsonObj1 = new JsonObject();
-        jsonObj1.add(fieldName, new JsonPrimitive(jsonValue1));
-        JsonObject jsonObj2 = new JsonObject();
-        jsonObj2.add(fieldName, new JsonPrimitive(jsonValue2));
-        JsonObject jsonObj3 = new JsonObject();
-        jsonObj3.add(fieldName, new JsonPrimitive(jsonValue3));
-        List<JsonObject> list = new LinkedList<>(Arrays.asList(jsonObj1, jsonObj2, jsonObj3));
+        final String fieldAge = "age";
+        int value = 10;
+        List<JsonObject> result = getValidatedIntData(query, fieldAge, value);
+        Assert.assertEquals(1, result.size());
+        JsonObject jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(value, jsonObject.get(fieldAge).getAsInt());
 
-        List<JsonObject> result = new JsonDataValidator().run(query, list);
-        Assert.assertEquals(2, result.size());
-        final JsonObject jsonObject = result.get(0);
-        Assert.assertTrue(jsonObject.has(fieldName));
-        Assert.assertEquals(jsonValue1, jsonObject.get(fieldName).getAsInt());
-        final JsonObject jsonObject2 = result.get(1);
-        Assert.assertTrue(jsonObject2.has(fieldName));
-        Assert.assertEquals(jsonValue2, jsonObject2.get(fieldName).getAsInt());
+        value = 9;
+        result = getValidatedIntData(query, fieldAge, value);
+        Assert.assertEquals(1, result.size());
+        jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(value, jsonObject.get(fieldAge).getAsInt());
+
+        double doubleValue = 9.9;
+        result = getValidatedDoubleData(query, fieldAge, doubleValue);
+        Assert.assertEquals(1, result.size());
+        jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(doubleValue, jsonObject.get(fieldAge).getAsDouble(), 2);
     }
 
     @Test
-    public void testLt() {
-        final String query = "age<10";
-        final String fieldName = "age";
-        final int jsonValue1 = 9;
-        final int jsonValue2 = 10;
-        JsonObject jsonObj1 = new JsonObject();
-        jsonObj1.add(fieldName, new JsonPrimitive(jsonValue1));
-        JsonObject jsonObj2 = new JsonObject();
-        jsonObj2.add(fieldName, new JsonPrimitive(jsonValue2));
-        List<JsonObject> list = new LinkedList<>(Arrays.asList(jsonObj1, jsonObj2));
+    public void testLessThanOrEqualForWrongInt() {
+        List<JsonObject> result = getValidatedIntData("age<=10", "age", 11);
+        Assert.assertEquals(0, result.size());
+    }
 
-        List<JsonObject> result = new JsonDataValidator().run(query, list);
+    @Test
+    public void testLessThanOrEqualForWrongDouble() {
+        List<JsonObject> result = getValidatedDoubleData("age<=10", "age", 10.1);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testLessThan() {
+        final String query = "age<10";
+        final String fieldAge = "age";
+        final int value = 9;
+        List<JsonObject> result = getValidatedIntData(query, fieldAge, value);
+
         Assert.assertEquals(1, result.size());
-        final JsonObject jsonObject = result.get(0);
-        Assert.assertTrue(jsonObject.has(fieldName));
-        Assert.assertEquals(jsonValue1, jsonObject.get(fieldName).getAsInt());
+        JsonObject jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(value, jsonObject.get(fieldAge).getAsInt());
+
+        double doubleValue = 9.9;
+        result = getValidatedDoubleData(query, fieldAge, doubleValue);
+        Assert.assertEquals(1, result.size());
+        jsonObject = result.get(0);
+        Assert.assertTrue(jsonObject.has(fieldAge));
+        Assert.assertEquals(doubleValue, jsonObject.get(fieldAge).getAsDouble(), 2);
+    }
+
+    @Test
+    public void testLessThanForWrongInt() {
+        List<JsonObject> result = getValidatedIntData("age<10", "age", 10);
+        Assert.assertEquals(0, result.size());
+    }
+
+    @Test
+    public void testLessThanForWrongDouble() {
+        List<JsonObject> result = getValidatedDoubleData("age<10", "age", 10.1);
+        Assert.assertEquals(0, result.size());
     }
 
     /**
@@ -196,8 +253,8 @@ public class JsonValidatorTest {
         obj1.add(fieldName1, new JsonPrimitive(value1));
         obj1.add(fieldName2, new JsonPrimitive(jsonValue2));
         list.add(obj1);
-
         List<JsonObject> result = new JsonDataValidator().run(query, list);
+
         Assert.assertEquals(1, result.size());
         final JsonObject jsonObject = result.get(0);
         Assert.assertTrue(jsonObject.has(fieldName1));
@@ -233,6 +290,7 @@ public class JsonValidatorTest {
             Assert.assertEquals(jsonValue2, jsonObject.get(fieldName2).getAsJsonPrimitive().getAsInt());
         }
     }
+
     /**
      * result = all false
      */
@@ -251,5 +309,22 @@ public class JsonValidatorTest {
 
         List<JsonObject> result = new JsonDataValidator().run(query, list);
         Assert.assertEquals(0, result.size());
+    }
+
+    private List<JsonObject> getValidatedIntData(String query, String fieldName, int jsonValue) {
+        JsonObject jsonObj = new JsonObject();
+        jsonObj.add(fieldName, new JsonPrimitive(jsonValue));
+        return validate(query, jsonObj);
+    }
+
+    private List<JsonObject> getValidatedDoubleData(String query, String fieldName, double jsonValue) {
+        JsonObject jsonObj = new JsonObject();
+        jsonObj.add(fieldName, new JsonPrimitive(jsonValue));
+        return validate(query, jsonObj);
+    }
+
+    private List<JsonObject> validate(String query, JsonObject jsonObj) {
+        List<JsonObject> list = new LinkedList<>(Collections.singletonList(jsonObj));
+        return new JsonDataValidator().run(query, list);
     }
 }
